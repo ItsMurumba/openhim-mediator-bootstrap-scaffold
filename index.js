45bbff9b27149b470900b88fd577c7b5a61493ce
KelvinWanyama <kevmurumba@gmail.com>
2020-03-05 14:12:27 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+import express from 'express'
+const app = express()
+app.all('*', (req, res) => {
+  res.send('Hello World')
+})
+app.listen(3000, () => {
+  console.log('Server listening on port 3000...')
+})
